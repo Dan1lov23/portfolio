28ba138eb65be83e501c85986f55e4ba16a1141f
kitik <svy1dan@gmail.com>
2025-10-13 17:09:28 +0300

    ///

--- src/pages/Home/page component/Projects/Password manager project/password manager description/PasswordManagerDescription.tsx
+++ src/pages/Home/page component/Projects/Password manager project/password manager description/PasswordManagerDescription.tsx
@@ -8,6 +8,7 @@ export default function PasswordManagerDescription() {
                     <p>A full-stack password manager application written in React and Express JS using a Sqlite3
                         database. Features include creating passwords, generating passwords based on specified
                         parameters, adding a password to favorites, and deleting a password.</p>
+                    <p>Technologies - (React, TSX, Express JS, Sqlite, JWT, Vite)</p>
                 </div>
             </div>
         </>
