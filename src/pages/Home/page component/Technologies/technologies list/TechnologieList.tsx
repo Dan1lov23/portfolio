61a48bf72cfd554b9807e6e3d9abde322f28e9a5
kitik <svy1dan@gmail.com>
2025-10-13 18:48:37 +0300

    ///

--- src/pages/Home/page component/Technologies/technologies list/TechnologieList.tsx
+++ src/pages/Home/page component/Technologies/technologies list/TechnologieList.tsx
@@ -1,32 +1,43 @@
 import "./technologiesList.css";
 import TechnologiesCart from "../technologie cart/TechnologiesCart.tsx";
 
-export default function TechnologiesList() {
+// Импорт изображений
+import htmlLogo from '../assets/technologies/htmlLogo.jpg';
+import cssLogo from '../assets/technologies/cssLogo.png';
+import scssLogo from '../assets/technologies/scssLogo.png';
+import jsLogo from '../assets/technologies/jsLogo.png';
+import tsLogo from '../assets/technologies/tsLogo.png';
+import reactLogo from '../assets/technologies/reactLogo.png';
+import reduxLogo from '../assets/technologies/reduxLogo.png';
+import zustandLogo from '../assets/technologies/zustandLogo.png';
+import viteLogo from '../assets/technologies/viteLogo.png';
+import gitLogo from '../assets/technologies/gitLogo.png';
+import githubLogo from '../assets/technologies/githubLogo.png';
+import cicdLogo from '../assets/technologies/cicdLogo.png';
 
+export default function TechnologiesList() {
     const technologiesArray = [
-        {imgSrc: "../src/pages/Home/page component/Technologies/imgs/htmlLogo.jpg", name: "HTML"},
-        {imgSrc: "../src/pages/Home/page component/Technologies/imgs/cssLogo.png", name: "CSS"},
-        {imgSrc: "../src/pages/Home/page component/Technologies/imgs/scssLogo.png", name: "SCSS"},
-        {imgSrc: "../src/pages/Home/page component/Technologies/imgs/jsLogo.png", name: "JS"},
-        {imgSrc: "../src/pages/Home/page component/Technologies/imgs/tsLogo.png", name: "TS"},
-        {imgSrc: "../src/pages/Home/page component/Technologies/imgs/reactLogo.png", name: "React"},
-        {imgSrc: "../src/pages/Home/page component/Technologies/imgs/reduxLogo.png", name: "Redux"},
-        {imgSrc: "../src/pages/Home/page component/Technologies/imgs/zustandLogo.png", name: "Zustand"},
-        {imgSrc: "../src/pages/Home/page component/Technologies/imgs/viteLogo.png", name: "Vite"},
-        {imgSrc: "../src/pages/Home/page component/Technologies/imgs/gitLogo.png", name: "Git"},
-        {imgSrc: "../src/pages/Home/page component/Technologies/imgs/githubLogo.png", name: "Github"},
-        {imgSrc: "../src/pages/Home/page component/Technologies/imgs/cicdLogo.png", name: "CI / CD"},
+        { imgSrc: htmlLogo, name: "HTML" },
+        { imgSrc: cssLogo, name: "CSS" },
+        { imgSrc: scssLogo, name: "SCSS" },
+        { imgSrc: jsLogo, name: "JS" },
+        { imgSrc: tsLogo, name: "TS" },
+        { imgSrc: reactLogo, name: "React" },
+        { imgSrc: reduxLogo, name: "Redux" },
+        { imgSrc: zustandLogo, name: "Zustand" },
+        { imgSrc: viteLogo, name: "Vite" },
+        { imgSrc: gitLogo, name: "Git" },
+        { imgSrc: githubLogo, name: "Github" },
+        { imgSrc: cicdLogo, name: "CI / CD" },
     ];
 
     return (
-        <>
-            <div className="technologiesList">
-                {technologiesArray.map((technology:any, index:number) => (
-                    <div key={index}>
-                        <TechnologiesCart imgSrc={technology.imgSrc} name={technology.name} />
-                    </div>
-                ))}
-            </div>
-        </>
-    )
+        <div className="technologiesList">
+            {technologiesArray.map((technology:any, index:number) => (
+                <div key={index}>
+                    <TechnologiesCart imgSrc={technology.imgSrc} name={technology.name} />
+                </div>
+            ))}
+        </div>
+    );
 }
